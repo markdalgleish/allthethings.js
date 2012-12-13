6f99b0881604caaa06a9a9cb150f9a109182230b
Ben Birch <bbirch@aconex.com>
2012-12-14 10:49:56 +1100

    Fix function name test for reduce call

--- dist/allthethings.js
+++ dist/allthethings.js
@@ -5,7 +5,7 @@
 (function(exports) {
 
 	exports.allTheThings = Function.prototype.allThe = Function.prototype.fromThe = function(things) {
-			this._allTheThings_func = this._allTheThings_func || (/^reduce|add|calculate/).test(this.name) ? things.reduce : (/^(filter|is)/).test(this.name) ? things.filter : things.map;
+			this._allTheThings_func = this._allTheThings_func || (/^(reduce|add|calculate)/).test(this.name) ? things.reduce : (/^(filter|is)/).test(this.name) ? things.filter : things.map;
 			return this._allTheThings_func.call(things, this);
 	};
 
